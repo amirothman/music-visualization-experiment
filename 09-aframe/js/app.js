3554989d300ff0cce245a13c821587eb54f1724a
Amir Othman <th3go2@gmail.com>
2016-08-16 11:44:42 +0200

    yay

--- 09-aframe/js/app.js
+++ 09-aframe/js/app.js
@@ -34,6 +34,7 @@ function finishedLoading(bufferList) {
     source.connect(context.destination);
     // console.log(source.buffer)
     analyser = context.createAnalyser();
+    analyser.fftSize = 64;
     // console.log(analyser.frequencyBinCount);
     frequencyData = new Uint8Array(analyser.frequencyBinCount);
     source.connect(analyser);
@@ -65,39 +66,49 @@ return window.requestAnimationFrame  ||
 
 var scene = d3.select("a-scene");
 var xScale = d3.scale.linear().domain([0,50]).range([-3,3]);
+
+var cirScale = d3.scale.linear().domain([0,63]).range([0,Math.PI*2])
+
 function renderChart() {
 //     // console.log()
      analyser.getByteFrequencyData(frequencyData);
-     var sliced_frequencyData = frequencyData.slice(950,1000)
+    //  var sliced_frequencyData = frequencyData.slice(950,1000)
     //  console.log(frequencyData)
      // <a-sphere position="0 1.25 -1" radius="1.25" color="#EF2D5E"></a-sphere>
      if (firstFrame === true){
 
-    bola = scene.selectAll("a-sphere.muzik").data(sliced_frequencyData)
+    bola = scene.selectAll("a-sphere.muzik").data(frequencyData)
                                .enter()
                                .append("a-sphere").classed("muzik",true)
-                               .attr("position",function(el,idx){
-                                 // console.log(idx);
-                                //  console.log(el);
-                                 return xScale(idx)+" "+Math.random()+" -1";
-                               })
-                               .attr("radius","0.25")
+                               .attr("position","0 2 -5")
+                               .attr("radius","0.2")
                                .attr("color","#EF2D5E");
       firstFrame = false;
-     }else{
+    }else{
        bola.attr("radius",function(el,idx){
-        //  console.log(idx);
-        var sp = d3.select(this);
-        var r = sp.attr("radius");
-        // console.log(frequencyData[idx]);
-         return frequencyData[idx]/1000;
+         if(frequencyData[idx]>50){
+           return frequencyData[idx]/400;
+         } else {
+           return 0;
+         }
        })
 
        bola.attr("position",function(el,idx){
-         var x = xScale(idx);
-         var y = frequencyData[idx]/100;
-         var z = frequencyData[idx]/100;
-         return x + ' '+y + ' '+ z;
+         if (frequencyData[idx] > 50){
+           var r = 3;
+          //  console.log(r,analyser.maxDecibels);
+           var theta = cirScale(idx);
+           var phi = frequencyData[idx]*(Math.PI*2/100)
+           var x = r*Math.cos(theta)*Math.sin(phi)*(1+Math.random()*0.3);
+           var y = r*Math.sin(theta)*Math.sin(phi)*(1+Math.random()*0.3);
+           var z = r*Math.cos(phi);
+          //  console.log(x,y,z)
+           return x + ' '+(y+2) + ' '+ (z-5);
+          // return "0 0 -100"
+        }else{
+          return "0 2 -5"
+        }
+
        })
      }
 
